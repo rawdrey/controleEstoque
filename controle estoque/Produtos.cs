c02d438faf76267b7e07720478bf3ec20ceadb52
Rawdrey <rawdrey@gmail.com>
2024-04-27 11:27:35 -0300

    class product criat add constrution class

--- controle estoque/Produtos.cs
+++ controle estoque/Produtos.cs
@@ -8,5 +8,17 @@ namespace controle_estoque
 {
     class Produtos
     {
+        public string SKU { get; set; };
+        public string Produto { get; set; };
+        public int Quantidade { get; set; };
+        public decimal PrecoCompra { get; set; };
+
+        public Produtos(string SKU, string Produto, int Quantidade, decimal PrecoCompra);
+        {
+        SKU = sku;
+            Produto = Produtos;
+            Quantidade = quantidade;
+            PrecoCompra = precocompra;
     }
+
 }
